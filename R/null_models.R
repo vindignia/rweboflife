fee1760e343f8d42a8f5a044159a6d3ad377a524
vindigna <vindigna@ethz.ch>
2023-08-24 16:30:50 +0200

    curveball null model

--- R/null_models.R
+++ R/null_models.R
@@ -1,21 +1,23 @@
 #' @NoRd
-pbc <-function(r, k, kring){
-  kneigh <- (r+k) %% kring
-  if(kneigh == 0) kneigh <- kring
+pbc <- function(r, k, kring) {
+  kneigh <- (r + k) %% kring
+  if (kneigh == 0)
+    kneigh <- kring
   return(kneigh)
 }
 
 #' @NoRd
-row_col_from_index <- function(index, nc){
+row_col_from_index <- function(index, nc) {
   j <- index %% nc
-  if(j == 0) j <- nc
-  i <- ceiling(index/nc)
-  return(c(i,j))
+  if (j == 0)
+    j <- nc
+  i <- ceiling(index / nc)
+  return(c(i, j))
 }
 
 #' @NoRd
-index_from_row_col <- function(i, j, nc){
-  index <- nc*(i-1) + j
+index_from_row_col <- function(i, j, nc) {
+  index <- nc * (i - 1) + j
   return(index)
 }
 
@@ -23,34 +25,33 @@ index_from_row_col <- function(i, j, nc){
 create_indexes <- function(nr, nc) {
   # create a vector with randomly selected indexes defining a 2x2 sub-matrix of M
   # select the first index at rnd
-  index1 <- ceiling(runif(1, min = 0, max = nr*nc))
+  index1 <- ceiling(runif(1, min = 0, max = nr * nc))
   pair <- row_col_from_index(index1, nc)
   i1 <- pair[1]
   j1 <- pair[2]
   # move along column j1 by rr
-  rr <- ceiling(runif(1, min = 0, max = nr-1))
+  rr <- ceiling(runif(1, min = 0, max = nr - 1))
   i2 <- pbc(rr, i1, nr)
   index2 <- index_from_row_col(i2, j1, nc) # 2nd index
   # move along row i1 by rr
-  rr <- ceiling(runif(1, min = 0, max = nc-1))
+  rr <- ceiling(runif(1, min = 0, max = nc - 1))
   j2 <- pbc(rr, j1, nc)
   index3 <- index_from_row_col(i1, j2, nc) # 3rd index
   index4 <- index_from_row_col(i2, j2, nc) # 4th index
 
-  return(c(index1,index2,index3,index4))
+  return(c(index1, index2, index3, index4))
 }
 
 #' @NoRd
-count_nonzero <- function(M){
-  MM <- as.matrix((M>0))
+count_nonzero <- function(M) {
+  MM <- as.matrix((M > 0))
   class(MM) <- "numeric"
   return(sum(MM))
 }
 
 
 #' @NoRd
-equifrequent_model <- function(M_in){
-
+equifrequent_model <- function(M_in) {
   rows <- nrow(M_in)
   columns <- ncol(M_in)
   number_ones <- count_nonzero(M_in)
@@ -59,19 +60,18 @@ equifrequent_model <- function(M_in){
   M_equif <- matrix(0, rows, columns)
 
   while (count_ones < number_ones) {
-
     x <- ceiling(runif(1, min = 0, max = rows))
     y <- ceiling(runif(1, min = 0, max = columns))
 
-    while (M_equif[x,y] == 1) {
-
+    while (M_equif[x, y] == 1) {
       x <- ceiling(runif(1, min = 0, max = rows))
       y <- ceiling(runif(1, min = 0, max = columns))
 
     }
 
-    M_equif[x,y] <- 1
-    count_ones <- count_ones + 1;
+    M_equif[x, y] <- 1
+    count_ones <- count_ones + 1
+
   }
 
   return(M_equif)
@@ -79,45 +79,46 @@ equifrequent_model <- function(M_in){
 
 
 #' @NoRd
-cell_model <- function(M_in){
-
+cell_model <- function(M_in) {
   rows <- nrow(M_in)
   columns <- ncol(M_in)
 
   # binarize M_in
-  M <- as.matrix((M_in>0))
+  M <- as.matrix((M_in > 0))
   class(M) <- "numeric"
 
   PR <- matrix(0, rows, 1)
   PC <- matrix(0, columns, 1)
   M_cell <- matrix(0, rows, columns)
 
-  for (i in 1:rows){
+  for (i in 1:rows) {
     number_ones <- 0
-    for (j in 1:columns){
-      if(M[i,j] == 1){
+    for (j in 1:columns) {
+      if (M[i, j] == 1) {
         number_ones <- number_ones + 1
       }
     }
-    PR[i] <- number_ones/columns
+    PR[i] <- number_ones / columns
   }
 
-  for (j in 1:columns){
+  for (j in 1:columns) {
     number_ones <- 0
-    for (i in 1:rows){
-      if(M[i,j] == 1){
+    for (i in 1:rows) {
+      if (M[i, j] == 1) {
         number_ones <- number_ones + 1
       }
     }
-    PC[j] <- number_ones/rows
+    PC[j] <- number_ones / rows
   }
 
-  for (i in 1:rows){
-    for (j in 1:columns){
-      p <- (PR[i]+PC[j])/2;
+  for (i in 1:rows) {
+    for (j in 1:columns) {
+      p <- (PR[i] + PC[j]) / 2
+
       r <- runif(1)
-      if( r < p ){
-        M_cell[i,j] <- 1;
+      if (r < p) {
+        M_cell[i, j] <- 1
+
       }
     }
 
@@ -133,9 +134,15 @@ swap_model = function(M_in, iter_max) {
   nr <- nrow(M_in) # number of rows
   nc <- ncol(M_in) # number of columns
 
-  if(is.null(iter_max)){
-    iter_max <- ceiling(10*sqrt(nr*nc))
-    warning(paste0("as the parameter iter_max was not assigned, the default value iter_max = ", iter_max," was assumed"))
+  if (is.null(iter_max)) {
+    iter_max <- ceiling(10 * sqrt(nr * nc))
+    warning(
+      paste0(
+        "as the parameter iter_max was not assigned, the default value iter_max = ",
+        iter_max,
+        " was assumed"
+      )
+    )
   }
 
   # initialize M (randomized matrix) iter loop
@@ -152,7 +159,6 @@ swap_model = function(M_in, iter_max) {
   M_swap <- B
 
   for (iter in 1:iter_max) {
-
     # attempt to swap as many 2x2 subsets as the matrix elements
     for (sweep in 1:nc * nr) {
       # Randomly select two distinct rows
@@ -181,19 +187,91 @@ swap_model = function(M_in, iter_max) {
 }
 
 
+#' @NoRd
+#' @import permute
+curveball_model = function(M_in, iter_max) {
+
+  # this algorithm is described in the manuscript
+  # Strona, G., et al.: A fast and unbiased procedure to randomize ecological binary matrices with fixed row and column totals.
+  # Nature Commun. vol. 5, 4114 (2014). DOI 10.1038/ncomms5114
+  # The present function is adapted from the code provided by the authors along with the manuscript
+
+  nr <- nrow(M_in) # number of rows
+  nc <- ncol(M_in) # number of columns
+
+  # binarize M_in into a logical matrix
+  M_in <- as.matrix((M_in > 0))
+  class(M_in) <- "numeric"
+
+  # locate the elements equal to 1 in each row, and store them in a list
+  M = if (nc >= nr) M_in else t(M_in)
+  hp = list()
+  for (row in 1:nrow(M)) {
+    hp[[row]] = (which(M[row, ] == 1))
+  }
+  l_hp = length(hp)
+
+
+  if (is.null(iter_max)) {
+    iter_max <- ceiling(5 * l_hp)
+    warning(
+      paste0(
+        "as the parameter iter_max was not assigned, the default value iter_max = ",
+        iter_max,
+        " was assumed"
+      )
+    )
+  }
+
+
+  for (iter in 1:iter_max) {
+    AB = sample(1:l_hp, 2)
+    a = hp[[AB[1]]]
+    b = hp[[AB[2]]]
+    ab = intersect(a, b)
+    l_ab = length(ab)
+    l_a = length(a)
+    l_b = length(b)
+    if ((l_ab %in% c(l_a, l_b)) == FALSE) {
+      tot = setdiff(c(a, b), ab)
+      l_tot = length(tot)
+      tot = sample(tot, l_tot, replace = FALSE, prob = NULL)
+      L = l_a - l_ab
+      hp[[AB[1]]] = c(ab, tot[1:L])
+      hp[[AB[2]]] = c(ab, tot[(L + 1):l_tot])
+    }
+
+  }
+
+  M_res = matrix(0, nrow(M), ncol(M))
+  for (row in 1:nrow(M)) {
+    M_res[row, hp[[row]]] = 1
+  }
+
+  M_curveball = if (nc >= nr) M_res else t(M_res)
+  return(M_curveball)
+}
+
+
 #' @export
-null_model <- function(M_in, iter_max = NULL, model = NULL){
+null_model <- function(M_in,
+                       iter_max = NULL,
+                       model = NULL) {
   # iter_max: empirically, the degree of overlapping with the original matrix
   # seems to converge exponentially with a decay constant equal to  2*sqrt(nrows*ncols);
   # after 5 decay constants we can assume the algorithm has converged.
 
   # Make sure we are working with a matrix
   M_in <- as.matrix(M_in)
-  if(is.null(model)){
-    stop("No type argument assigned for the null model. Please choose one from the list: \"swap\", \"equifrequent\", \"cell\"")
+  if (is.null(model)) {
+    stop(
+      "No type argument assigned for the null model. Please choose one from the list: \"swap\", \"equifrequent\", \"cell\", \"curveball\""
+    )
   } else {
-    if (model == "swap"){
+    if (model == "swap") {
       M_out <- swap_model(M_in, iter_max)
+    } else if (model == "curveball") {
+      M_out <- curveball_model(M_in, iter_max)
     } else if (model == "equifrequent") {
       M_out <- equifrequent_model(M_in)
     }  else if (model == "cell") {
--- tests/testthat/test-null_models.R
+++ tests/testthat/test-null_models.R
@@ -1,4 +1,4 @@
-test_that("check that the number of 1 values is preserved in the null models", {
+test_that("check that the number of 1 values is preserved in the equifrequent null model", {
 
   n <- 75 # number of rows
   m <- 92  # number of columns
@@ -10,16 +10,11 @@ test_that("check that the number of 1 values is preserved in the null models", {
     }
   }
 
-  M_swap <- null_model(M, iter_max = ceiling(3*sqrt(nrow(M)*ncol(M))), model = "swap")
-  expect_equal(sum(M_swap), sum(M))
-
   M_equif <- null_model(M, model = "equifrequent")
   expect_equal(sum(M_equif), sum(M))
 
-  # M_cell <- null_model(M, model = "cell")
-  # expect_equal(sum(M_cell), sum(M))
-
   # the cell model does not preserve the number of ones
+  # the swap and curveball models fulfill the more stringent condition of preserving the row and column totals => checked later on
 
 })
 
@@ -54,6 +49,40 @@ test_that("check that the row and columns totals are preserved in the swap null 
   for(j in seq(1,ncol(M))){
     cols_tot_rnd <- append(cols_tot_rnd, sum(M_swap[,j]))
     cols_tot_original <- append(cols_tot_original, sum(M_swap[,j]))
+  }
+  expect_true(all(cols_tot_rnd == cols_tot_original))
+
+})
+
+test_that("check that the row and columns totals are preserved in the curveball null model", {
+
+  n <- 113 # number of rows
+  m <- 92  # number of columns
+  M <- matrix(0,n,m)
+
+  for(i in 1:n){
+    for (j in 1:m){
+      M[i,j] <- floor(runif(1, min = 0, max = 1.5)) # binary matrix
+    }
+  }
+
+  M_curveball <- null_model(M, iter_max = ceiling(3*sqrt(nrow(M)*ncol(M))), model = "curveball")
+
+  # check row total
+  rows_tot_rnd <- c()
+  rows_tot_original <- c()
+  for(i in seq(1,nrow(M))){
+    rows_tot_rnd <- append(rows_tot_rnd, sum(M_curveball[i,]))
+    rows_tot_original <- append(rows_tot_original, sum(M_curveball[i,]))
+  }
+  expect_true(all(rows_tot_rnd == rows_tot_original))
+
+  # check col total
+  cols_tot_rnd <- c()
+  cols_tot_original <- c()
+  for(j in seq(1,ncol(M))){
+    cols_tot_rnd <- append(cols_tot_rnd, sum(M_curveball[,j]))
+    cols_tot_original <- append(cols_tot_original, sum(M_curveball[,j]))
   }
   expect_true(all(cols_tot_rnd == cols_tot_original))
 
